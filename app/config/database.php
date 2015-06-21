9492cb9e39de62bf34dd97b24603b66fc18b4b1d
Muhamad Habibi Azmi <m.habibi.azmi.mha@gmail.com>
2015-06-21 14:23:50 +0700

    ubah password
    ubah

--- app/config/database.php
+++ app/config/database.php
@@ -57,7 +57,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'ams',
 			'username'  => 'ams',
-			'password'  => 'ams',
+			'password'  => '',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
